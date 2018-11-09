debf79f5bd24df4635e978ed2433a5b57db7d005
Andrzej Szmajnta <andrzej.szmajnta.dev@gmail.com>
2018-11-09 10:50:07 +0100

    Damn rn

--- android/app/src/main/java/com/meetluna/luna/MainApplication.java
+++ android/app/src/main/java/com/meetluna/luna/MainApplication.java
@@ -1,66 +1,57 @@
 package com.meetluna.luna;
 
 import android.app.Application;
-
+import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.crashlytics.android.Crashlytics;
 import com.facebook.react.ReactApplication;
-import org.devio.rn.splashscreen.SplashScreenReactPackage;
-import com.reactcommunity.rnlanguages.RNLanguagesPackage;
-import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
-import com.smixx.fabric.FabricPackage;
-import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
-
-import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
+import com.reactcommunity.rnlanguages.RNLanguagesPackage;
+import com.smixx.fabric.FabricPackage;
+import io.fabric.sdk.android.Fabric;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 
 import java.util.Arrays;
 import java.util.List;
 
-import com.reactcommunity.rnlanguages.RNLanguagesPackage;
-import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
-import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
-import com.smixx.fabric.FabricPackage;
-
-import io.fabric.sdk.android.Fabric;
-
 public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.<ReactPackage>asList(
+                    new MainReactPackage(),
+                    new SplashScreenReactPackage(),
+                    new RNLanguagesPackage(),
+                    new RNI18nPackage(),
+                    new ReactNativeConfigPackage(),
+                    new FabricPackage()
+            );
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+            return "index";
+        }
+    };
+
     @Override
-    public boolean getUseDeveloperSupport() {
-      return  BuildConfig.DEBUG;
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
     }
 
     @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-          new SplashScreenReactPackage(),
-          new RNLanguagesPackage(),
-          new RNI18nPackage(),
-          new ReactNativeConfigPackage(),
-          new FabricPackage()
-      );
+    public void onCreate() {
+        super.onCreate();
+        Fabric.with(this, new Crashlytics());
+        SoLoader.init(this, /* native exopackage */ false);
     }
-
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
-
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    Fabric.with(this, new Crashlytics());
-    SoLoader.init(this, /* native exopackage */ false);
-  }
 }
