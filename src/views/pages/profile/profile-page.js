d57cf56d4be65dc963fd45cf71353c3bfcbe1a66
Andrzej Szmajnta <andrzej.szmajnta.dev@gmail.com>
2018-11-16 15:07:04 +0100

    Tururu

--- src/views/pages/profile/profile-page.js
+++ src/views/pages/profile/profile-page.js
@@ -1,20 +1,36 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 import { connect } from 'react-redux'
+import HeaderImageScrollView, {
+	TriggeringView
+} from 'react-native-image-header-scroll-view'
 
 export class ProfilePage extends React.Component {
 	render() {
-		return <View />
+		return (
+			<HeaderImageScrollView
+				headerImage={{ uri: this.props.profile.avatarUrl }}
+			>
+				<View style={{ height: 1000 }}>
+					<TriggeringView onHide={() => {}}>
+						<Text>Scroll Me!</Text>
+					</TriggeringView>
+				</View>
+			</HeaderImageScrollView>
+		)
 	}
 }
 
 ProfilePage.propTypes = {
-	navigation: PropTypes.object.isRequired
+	navigation: PropTypes.object.isRequired,
+	profile: PropTypes.object.isRequired
 }
 
 const mapStateToProps = () => {
-	return {}
+	return {
+		profile: {}
+	}
 }
 
 const mapDispatchToProps = () => {
