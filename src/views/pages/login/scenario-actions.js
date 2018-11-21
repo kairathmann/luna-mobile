76a1035d3931b7bc14d8eaf97857fab5c00b57e6
Dariusz Górak <dariusz.gorak@rspective.pl>
2018-11-21 10:10:38 +0100

    Change login action to redirect user to HomePage when user profile is complete, otherwise redirect user to appropiate Flow Page. Moved loading of recommendations to componentDidMount inside recommendations-page.js

--- src/views/pages/login/scenario-actions.js
+++ src/views/pages/login/scenario-actions.js
@@ -4,6 +4,7 @@ import { PAGES_NAMES } from '../../../navigation'
 import { navigationService } from '../../../services'
 import { signinError, signinSuccess } from '../../../store/auth/actions'
 import { fetchProfileSuccess } from '../../../store/profile/actions'
+import { PROFILE_STATE } from '../../../enums'
 
 export function login({ email, password }) {
 	return async dispatch => {
@@ -12,8 +13,20 @@ export function login({ email, password }) {
 			const loginResult = await api.signin({ email, password })
 			dispatch(signinSuccess(loginResult))
 			const profileResponse = await api.fetchSelf()
-			dispatch(fetchProfileSuccess(profileResponse.data.data))
-			navigationService.navigate(PAGES_NAMES.PROFILE)
+			const profileData = profileResponse.data.data
+			dispatch(fetchProfileSuccess(profileData))
+			if (profileData.state === PROFILE_STATE.INCOMPLETE) {
+				// Profile is marked as COMPLETE as soon as user has filled up
+				// name, date of birth and partner preferences
+				// So, if first name already exists then go to partner preferences page
+				// Otherwise go to first page of the flows
+				const flowPageToNavigateTo = profileData.firstName
+					? PAGES_NAMES.FLOW_GENDER_SEXUALITY
+					: PAGES_NAMES.FLOW_NAME_BIRTHDAY
+				navigationService.navigate(flowPageToNavigateTo)
+			} else {
+				navigationService.navigate(PAGES_NAMES.HOME_PAGE)
+			}
 		} catch (error) {
 			dispatch(signinError(getErrorDataFromNetworkException(error)))
 		} finally {
