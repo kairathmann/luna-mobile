a454d4360015c019e2cb44a95e874e91791eca71
Andrzej Szmajnta <andrzej.szmajnta.dev@gmail.com>
2018-11-23 14:22:37 +0100

    Merge with logout, remove delay on logout

--- src/views/pages/profile/scenario-actions.js
+++ src/views/pages/profile/scenario-actions.js
@@ -2,8 +2,8 @@ import api from '../../../api/api'
 import { PAGES_NAMES } from '../../../navigation'
 import { navigationService } from '../../../services'
 import { logoutUser } from '../../../store/auth/actions'
-import { clearData as clearRecommendations } from '../../../store/recommendations/actions'
 import { clearData as clearProfile } from '../../../store/profile/actions'
+import { clearData as clearRecommendations } from '../../../store/recommendations/actions'
 
 export function logout() {
 	return async dispatch => {
@@ -11,12 +11,10 @@ export function logout() {
 			navigationService.navigate(PAGES_NAMES.LOADER)
 			await api.logout()
 		} finally {
-			setTimeout(() => {
-				navigationService.navigate(PAGES_NAMES.WELCOME_PAGE)
-				dispatch(clearRecommendations())
-				dispatch(clearProfile())
-				dispatch(logoutUser())
-			}, 2500)
+			navigationService.navigate(PAGES_NAMES.WELCOME_PAGE)
+			dispatch(clearRecommendations())
+			dispatch(clearProfile())
+			dispatch(logoutUser())
 		}
 	}
 }
