1456cdfca6c95c745e24b36a458edb5f12873774
Andrzej Szmajnta <andrzej.szmajnta.dev@gmail.com>
2018-11-26 15:11:22 +0100

    REmove console.log

--- src/views/pages/edit/scenario-actions.js
+++ src/views/pages/edit/scenario-actions.js
@@ -10,7 +10,6 @@ import {
 } from '../../../store/profile/actions'
 
 export function uploadChanges(changes, avatar) {
-	console.log({ changes, avatar })
 	return async dispatch => {
 		try {
 			let profileToSave = { ...changes }
