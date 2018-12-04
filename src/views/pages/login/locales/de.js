3fbb37cefa3b9e1337d9ca137e5d7d2b3729ec5d
Andrzej Szmajnta <andrzej.szmajnta.dev@gmail.com>
2018-12-04 16:14:04 +0100

    Show toasts when disable and reactivated

--- src/views/pages/login/locales/de.js
+++ src/views/pages/login/locales/de.js
@@ -2,5 +2,6 @@ export default {
 	title: 'Log in',
 	password_with_info: 'Password',
 	sign_in: 'Sign in',
-	forget_password: 'Forgot password?'
+	forget_password: 'Forgot password?',
+	reactivated: "Welcome back, we've reactivated your account"
 }
