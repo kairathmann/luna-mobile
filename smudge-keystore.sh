9ec99ec0651fcab408bfc848d95f6e84da45a369
Tadeusz Andrzej Kadłubowski <yess@hell.org.pl>
2019-01-21 13:41:52 +0100

    /bin/bash for clean and smudge (#152)

--- smudge-keystore.sh
+++ smudge-keystore.sh
@@ -1,9 +1,10 @@
-#! /bin/sh -f
+#!/bin/bash -fx
 if
 	[ "${LUNA_KEYSTORE_PASSWORD}" == "" ] || [ "${LUNA_KEYSTORE_KEY_PASSWORD}" == "" ]
 then
 	exit 1
 fi
+
 ESCAPED_KEYSTORE_PASSWORD=$(echo ${LUNA_KEYSTORE_PASSWORD} | sed 's/\([\/\\\-\|\.\*\[\}]\)/\\\1/g')
 ESCAPED_KEYSTORE_KEY_PASSWORD=$(echo ${LUNA_KEYSTORE_KEY_PASSWORD} | sed 's/\([\/\\\-\|\.\*\[\}]\)/\\\1/g')
 sed -e "s/@@LUNA_KEYSTORE_PASSWORD@@/$ESCAPED_KEYSTORE_PASSWORD/" -e "s/@@LUNA_KEYSTORE_KEY_PASSWORD@@/$ESCAPED_KEYSTORE_KEY_PASSWORD/"
